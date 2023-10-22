d2b97a9523896d401748ce5e415afaf39b3138c3
Jekabs Gritans <jekabs.gritans@giraffe360.com>
2023-10-22 22:27:00 +0200

    pseudo legal move validation without special moves

--- engine/RuleEngine.java
+++ engine/RuleEngine.java
@@ -9,39 +9,189 @@ import java.util.List;
  * All chess rules are implemented here.
  */
 public class RuleEngine {
+
+    //TODO filter for check
+
+    // valid 1d move directions for each piece
+    private static final int[] WHITE_PAWN_DIRS = new int[] {-1, 1, 8}; 
+    private static final int[] BLACK_PAWN_DIRS = new int[] {-1, 1, -8};
+    private static final int[] KNIGHT_DIRS = new int[] {-17, -15, -10, -6, 6, 10, 15, 17};
+    private static final int[] BISHOP_DIRS = new int[] {-9, -7, 7, 9};
+    private static final int[] ROOK_DIRS = new int[] {-8, -1, 1, 8}; // TODO add castling
+    private static final int[] QUEEN_DIRS = new int[] {-9, -8, -7, -1, 1, 7, 8, 9};
+    private static final int[] KING_DIRS = new int[] {-9, -8, -7, -1, 1, 7, 8, 9}; // TODO add castling, filter by check separately
+
     /**
-     * Gets all valid moves for a player.
+     * Gets all valid moves that the current player can make.
+     * @param state the game state
+     * @return list of valid moves
      */
     public List<Move> getValidMoves(GameState state) {
-        List<Move> moves = new ArrayList<Move>(0);
-        boolean isWhite = state.isWhiteMove();
         ChessBoard board = state.board();
+        boolean isWhiteMove = state.isWhiteMove();
+        List<Move> moves = new ArrayList<Move>(0);
 
-        for (int rowIdx = 0; rowIdx < 8; rowIdx++) {
-            for (int colIdx = 0; colIdx < 8; colIdx++) {
-                byte piece = board.getPiece(rowIdx, colIdx);
-                ChessBoard.Position pos = new ChessBoard.Position(rowIdx, colIdx);
+        for (int i = 0; i < 64; i++) {
+            byte piece = board.getPiece(i);
 
-                // dummy moves, allow 1 step in any direction
-                if (!ChessPiece.isEmpty(piece) && ChessPiece.isWhite(piece) == isWhite) {
-                    // horizontal
-                    if (board.checkInBounds(rowIdx, colIdx - 1)) moves.add(new Move(pos, new ChessBoard.Position(rowIdx, colIdx - 1)));
-                    if (board.checkInBounds(rowIdx, colIdx + 1)) moves.add(new Move(pos, new ChessBoard.Position(rowIdx, colIdx + 1)));
-
-                    // vertical
-                    if (board.checkInBounds(rowIdx + 1, colIdx)) moves.add(new Move(pos, new ChessBoard.Position(rowIdx + 1, colIdx)));
-                    if (board.checkInBounds(rowIdx - 1, colIdx)) moves.add(new Move(pos, new ChessBoard.Position(rowIdx - 1, colIdx)));
-
-                    // diagonal
-                    if (board.checkInBounds(rowIdx + 1, colIdx + 1)) moves.add(new Move(pos, new ChessBoard.Position(rowIdx + 1, colIdx + 1)));
-                    if (board.checkInBounds(rowIdx - 1, colIdx - 1)) moves.add(new Move(pos, new ChessBoard.Position(rowIdx - 1, colIdx - 1)));
-                    if (board.checkInBounds(rowIdx + 1, colIdx - 1)) moves.add(new Move(pos, new ChessBoard.Position(rowIdx + 1, colIdx - 1)));
-                    if (board.checkInBounds(rowIdx - 1, colIdx + 1)) moves.add(new Move(pos, new ChessBoard.Position(rowIdx - 1, colIdx + 1)));
-                }
+            switch (ChessPiece.getType(piece)) {
+                case ChessPiece.Empty:
+                    break;
+                case ChessPiece.Pawn:
+                    addValidPawnMoves(i, board, isWhiteMove, moves);
+                    break;
+                case ChessPiece.Knight:
+                    addValidKnightMoves(i, board, isWhiteMove, moves);
+                    break;
+                case ChessPiece.Bishop:
+                    addValidBishopMoves(i, board, isWhiteMove, moves);
+                    break;
+                case ChessPiece.Rook:
+                    addValidRookMoves(i, board, isWhiteMove, moves);
+                    break;
+                case ChessPiece.Queen:
+                    addValidQueenMoves(i, board, isWhiteMove, moves);
+                    break;
+                case ChessPiece.King:
+                    addValidKingMoves(i, board, isWhiteMove, moves);
+                    break;
+                default:
+                    break;
             }
         }
 
         return moves;
+    }
+
+    /**
+     * Adds all valid pawn moves to the list.
+     * @param pos 1d position of the piece
+     * @param board the board
+     * @param isWhite whether the piece is white
+     * @param moves list of valid moves
+     */
+    private void addValidPawnMoves(int pos, ChessBoard board, boolean isWhite, List<Move> moves) {
+        int[] dirs = isWhite ? WHITE_PAWN_DIRS : BLACK_PAWN_DIRS;
+        addValidNonSlidingMoves(pos, dirs, board, isWhite, moves);
+
+        //TODO double move
+
+        //TODO en passant
+    }
+
+    /**
+     * Adds all valid knight moves to the list.
+     * @param pos 1d position of the piece
+     * @param board the board
+     * @param isWhite whether the piece is white
+     * @param moves list of valid moves
+     */
+    private void addValidKnightMoves(int pos, ChessBoard board, boolean isWhite, List<Move> moves) {
+        addValidNonSlidingMoves(pos, KNIGHT_DIRS, board, isWhite, moves);
+    }
+
+    /**
+     * Adds all valid bishop moves to the list.
+     * @param pos 1d position of the piece
+     * @param board the board
+     * @param isWhite whether the piece is white
+     * @param moves list of valid moves
+     */
+    private void addValidBishopMoves(int pos, ChessBoard board, boolean isWhite, List<Move> moves) {
+        addValidSlidingMoves(pos, BISHOP_DIRS, board, isWhite, moves);
+    }
+
+    /**
+     * Adds all valid rook moves to the list.
+     * @param pos 1d position of the piece
+     * @param board the board
+     * @param isWhite whether the piece is white
+     * @param moves list of valid moves
+     */
+    private void addValidRookMoves(int pos, ChessBoard board, boolean isWhite, List<Move> moves) {
+        addValidSlidingMoves(pos, ROOK_DIRS, board, isWhite, moves);
+    }
+
+    /**
+     * Adds all valid queen moves to the list.
+     * @param pos 1d position of the piece
+     * @param board the board
+     * @param isWhite whether the piece is white
+     * @param moves list of valid moves
+     */
+    private void addValidQueenMoves(int pos, ChessBoard board, boolean isWhite, List<Move> moves) {
+        addValidSlidingMoves(pos, QUEEN_DIRS, board, isWhite, moves);
+    }
+
+    /**
+     * Adds all valid king moves to the list.
+     * @param pos 1d position of the piece
+     * @param board the board
+     * @param isWhite whether the piece is white
+     * @param moves list of valid moves
+     */
+    private void addValidKingMoves(int pos, ChessBoard board, boolean isWhite, List<Move> moves) {
+        addValidNonSlidingMoves(pos, KING_DIRS, board, isWhite, moves);
+
+        //TODO castling
+    }
+
+    /**
+     * Adds all valid sliding moves for a piece.
+     * @param pos 1d position of the piece
+     * @param directions array of 1d directions
+     * @param board the board
+     * @param isWhite whether the piece is white
+     * @param moves list of valid moves
+     */
+    private void addValidSlidingMoves(int pos, int[] directions, ChessBoard board, boolean isWhite, List<Move> moves) {
+        for (int dir : directions) {
+            int newPos = pos + dir;
+
+            // while in bounds
+            while (newPos >= 0 && newPos < 64) {
+                byte piece = board.getPiece(newPos);
+
+                if (piece == ChessPiece.Empty) {
+                    moves.add(board.new Move(pos, newPos));
+                } else {
+                    if (ChessPiece.isWhite(piece) != isWhite) {
+                        moves.add(board.new Move(pos, newPos));
+                    }
+
+                    // continue until blocked by piece
+                    break;
+                }
+                newPos += dir;
+            }
+        }
+    }
+
+    /**
+     * Adds all valid non-sliding moves for a piece.
+     * @param pos 1d position of the piece
+     * @param directions array of 1d directions
+     * @param board the board
+     * @param isWhite whether the piece is white
+     * @param moves list of valid moves
+     */
+    private void addValidNonSlidingMoves(int pos, int[] directions, ChessBoard board, boolean isWhite, List<Move> moves) {
+        for (int dir : directions) {
+            int newPos = pos + dir;
+
+            // while in bounds
+            if (newPos >= 0 && newPos < 64) {
+                byte piece = board.getPiece(newPos);
+
+                if (piece == ChessPiece.Empty) {
+                    moves.add(board.new Move(pos, newPos));
+                } else {
+                    if (ChessPiece.isWhite(piece) != isWhite) {
+                        moves.add(board.new Move(pos, newPos));
+                    }
+                }
+            }
+        }
     }
 
     /**
